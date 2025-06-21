cd40d6f83026cb7ddb757965956ade7c0b95ac00
Ana Carolina <anacarolinaaps@gmail.com>
2025-06-21 18:04:33 -0300

    [feature] Adicionado ROUTES no projeto
    - Adicionado o routes no projeto;
    - Conteúdo do App.tsx movido para pages>home.tsx;

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
 import './App.css'
-import Computer from './assets/computer.png';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
 
   return (
-    <> 
-      <div className="row">
-        <div className="col-6">
-          <h1 className='text-3xl titulo'>Olá! Sou <br/><span className='primary'>Ana Carolina</span></h1>
-          <h2 className='text-xl subtitulo secondary'>Desenvolvedora Full Stack</h2>
-          <p className='text-lg'>
-            Desenvolvedora Full Stack com experiência em sistemas ERP e vitrines virtuais, com forte atuação em PHP (Laravel) e bancos de dados MySQL. Atualmente ampliando conhecimentos em frameworks modernos como React e Vue.js para retomar atuação na área de programação.
-          </p>
-        </div>
-        <div className="col-6">
-          <img src={Computer} alt="" className='responsive-img'/>
-        </div>
-      </div>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   )
 }
 
