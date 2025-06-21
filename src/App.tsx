ae413ef93b09b7922558f5b1e517ee393b001d56
Ana Carolina <anacarolinaaps@gmail.com>
2025-06-21 18:11:20 -0300

    [feature] Adicionadas Páginas para o Projeto
    - Criado pages>sobre + rota para ser trabalhada mais tarde;
    - Criado pages>projetos + rota para ser trabalhada mais tarde;
    - Criado pages>projetodetalhes para ser trabalhada mais tarde;

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import Inicio from './pages/Home';
+import Sobre from './pages/Sobre';
+import Projetos from './pages/Projetos';
 
 function App() {
 
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Inicio />} />
+        <Route path="/sobre" element={<Sobre />} />
+        <Route path="/projetos" element={<Projetos />} />
       </Routes>
     </Router>
   )
---
+++ src/pages/ProjetoDetalhes.tsx
@@ -0,0 +1,14 @@
+function ProjetoDetalhes() {
+  return (
+    <> 
+      <div className="row">
+        <div className="col">
+          <h1 className='text-3xl titulo secondary'>Projetos</h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ProjetoDetalhes;
+
