55125a1d89b52a92f0cbbd301e167736dd6b4cd4
Ana Carolina <anacarolinaaps@gmail.com>
2025-06-21 21:48:27 -0300

    [chore] Separação dos Arquivos de CSS
    - Separado arquivos CSS conforme sua utilidade;
    - Alterados arquivos que utilizam o CSS para ajustar a troca de alguns nomes;

--- src/pages/ProjetoDetalhes.tsx
+++ src/pages/ProjetoDetalhes.tsx
@@ -3,7 +3,7 @@ function ProjetoDetalhes() {
     <> 
       <div className="row">
         <div className="col">
-          <h1 className='text-3xl titulo secondary'>Projetos</h1>
+          <h1 className='text-3xl heading-main text-secondary'>Projetos</h1>
         </div>
       </div>
     </>
--- src/pages/Projetos.tsx
+++ src/pages/Projetos.tsx
@@ -4,7 +4,7 @@ function Projetos() {
     <> 
       <div className="row">
         <div className="col">
-          <h1 className='text-3xl titulo secondary'>Projetos</h1>
+          <h1 className='text-3xl heading-main text-secondary'>Projetos</h1>
         </div>
       </div>
     </>
