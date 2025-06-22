850da580b12ef876e5790c4c175e814b00d354b6
Ana Carolina <anacarolinaaps@gmail.com>
2025-06-22 17:10:50 -0300

    [feature] Criado Arquivos de Dados para Projeto
    - Criado Arquivo para os dados referentes a educação;
    - Criado Arquivo para os dados referentes a experiencias;
    - Criado Arquivo para os dados referentes a habilidades;
    - Criado Arquivo para os dados referentes a projetos;
    - Criado Arquivo para os dados referentes a sobre mim;
    - Alterado a identação do arquivo de dados do RESUMO;

---
+++ src/data/sobremim.js
@@ -0,0 +1,12 @@
+const sobremimData = {
+    resumo: "Minha trajetória profissional na tecnologia começou em 2010 e ao longo dos anos tive a oportunidade de trabalhar com diversas áreas, como ensino, desenvolvimento desktop e web, além de testes de software e análise. Tive oportunidade de trabalhar com diversas linguagens, incluindo tecnologias proprietárias.\nNos últimos sete anos, desenvolvi um sistema ERP completo em PHP (laravel) apesar da minha principal aérea de atuação estar focada na administrativa e financeiro da empresa em que trabalho. Pude com isso, informatizar 100% os processos logísticos e financeiros da empresa.\nNo último ano venho me dedicando ao front-end, aprimorando minhas habilidades em React e VueJS. Estou muito animada para atuar como desenvolvedora júnior. Amo aprender coisas novas, tanto no âmbito profissional quanto pessoal. Compartilhar conhecimento e construir soluções que facilitem processos é algo que considero muito satisfatório, seja causando pequenos ou grandes impactos.",  
+    linkcv: "/assets/Ana-CV.pdf",
+    itens: [
+        { nome: "linkedin", link:"https://www.linkedin.com/in/ana-carolina-aps/", icone: "/assets/icons/brand-linkedin.svg" },
+        { nome: "github", link:"https://github.com/AnaCarolinaAPS", icone: "/assets/icons/brand-github.svg" },
+        { nome: "email", link:"mailto:anacarolinapv@gmail.com", icone: "/assets/icons/mail.svg" },
+        { nome: "whatsapp", link:"https://wa.me/555180194413", icone: "/assets/icons/brand-whatsapp.svg" },
+    ]          
+};
+
+export default sobremimData;
