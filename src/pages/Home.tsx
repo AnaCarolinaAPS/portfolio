c7ab3a952cc603923bd73135c4fb4594062de4a3
Ana Carolina dos Anjos <anacarolinaaps@gmail.com>
2025-06-23 14:55:29 -0300

    [refactor] (home) Tipados os maps dos objetos de data
    - Criado as interfaces (tipado) os objetos que vem dos dados;

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,7 +8,32 @@ import { NavLink } from 'react-router-dom';
 
 function Home() {
 
+    const paragrafos: string[] = sobremimData.resumo.split('\n');
     const projetosDestaque = projetosData.slice(0, 3); // Coleção dos 3 primeiros projetos (DESTAQUE)
+    
+    interface ContatoItem {
+        nome: string;
+        link: string;
+        icone: string;
+    }
+
+    interface HabilidadeItem {
+        nome: string;
+        icone: string;
+    }
+
+    interface HabilidadeCategoria {
+        categoria: string;
+        itens: HabilidadeItem[];
+    }
+
+    interface ProjetosDataType {
+        id: string;
+        titulo: string;
+        imagem: string;
+        resumo: string;
+        descricao: string;
+    }
     
     return (
         <> 
@@ -29,7 +54,7 @@ function Home() {
                 <div className="row text-center">
                     <div className="col">
                         <h2 className='text-3xl text-primary mb-5'>Sobre Mim</h2>
-                        {sobremimData.resumo.split('\n').map((paragrafo, index) => (
+                        {paragrafos.map((paragrafo, index) => (
                             <p key={index} className="text-lg text-justify">{paragrafo}</p>
                         ))}
                     </div>       
@@ -61,9 +86,9 @@ function Home() {
                 </div>
                 <div className="row mt-4">
                     <div className="col-12 align-center">
-                        {sobremimData.itens.map(({ nome, link, icone }, index) => (
+                        {sobremimData.itens.map(( item: ContatoItem , index: number) => (
                             <div key={index} className="sobremim-icon ml-2 text-primary">
-                                <a href={link} target="_blank"><img src={icone} /></a>
+                                <a href={item.link} target="_blank"><img src={item.icone} /></a>
                             </div>
                         ))}
                     </div>
@@ -76,16 +101,16 @@ function Home() {
                         <h2 className='text-3xl text-secondary mb-5'>Habilidades</h2>
                     </div>       
                 </div>
-                {habilidadeData.map(({ categoria, itens }, index) => (
+                {habilidadeData.map(( hab: HabilidadeCategoria, index: number) => (
                     <div key={index} className="row mt-5">
                         <div className="col-6 mb-4">
-                            <h2 className='text-xl text-uppercase'>{categoria}</h2>
+                            <h2 className='text-xl text-uppercase'>{hab.categoria}</h2>
                         </div>
                         <div className="col-6 habilidade">
-                            {itens.map(({ nome, icone }, i) => (
+                            {hab.itens.map((item: HabilidadeItem, i) => (
                                 <div key={i} className="habilidade-kit">
-                                    <img src={icone} alt={nome} className="habilidade-icon mb-1" />
-                                    <span>{nome}</span>
+                                    <img src={item.icone} alt={item.nome} className="habilidade-icon mb-1" />
+                                    <span>{item.nome}</span>
                                 </div>
                             ))}
                         </div>
@@ -101,7 +126,7 @@ function Home() {
                 </div>
 
                 <div className="row text-center">
-                    {projetosDestaque.map((proj) => (
+                    {projetosDestaque.map((proj: ProjetosDataType) => (
                         <div className="col-4 mb-4">
                         <ProjectCard
                             key={proj.id}
