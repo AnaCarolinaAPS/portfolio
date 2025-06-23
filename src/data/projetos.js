200354cc188ac84148c8d8534e31e4c4649ae050
Ana Carolina <anacarolinaaps@gmail.com>
2025-06-22 22:09:44 -0300

    [feature] Adicionado conteúdo em data/projetos
    - Adicionado conteúdo provisório para data/projetos.js;

---
+++ src/data/projetos.js
@@ -0,0 +1,40 @@
+const projetosData = [
+    {
+        id: "powertrade",
+        titulo: "PowerTrade.py",
+        imagem: "/assets/images/powertrade.png", 
+        resumo: "Sistema completo de logística com controle financeiro, funcionários e faturação.",
+        descricao: "Desenvolvedora Full Stack com experiência em sistemas ERP e vitrines virtuais, com forte atuação em PHP (Laravel) e bancos de dados MySQL. Atualmente ampliando conhecimentos em frameworks modernos como React e Vue.js para retomar atuação na área de programação.",
+        tecnologias: [
+            { nome: "React", icone: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" },
+            { nome: "Bootstrap", icone: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original.svg" },
+            { nome: "Vue.js", icone: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vuejs/vuejs-original.svg" },
+        ]
+    },
+    {
+        id: "capacit",
+        titulo: "Capacit",
+        imagem: "/assets/images/capacit.png", 
+        resumo: "Sistema para uma escola de informática com controle de alunos, turmas e pagamentos.",
+        descricao: "Desenvolvedora Full Stack com experiência em sistemas ERP e vitrines virtuais, com forte atuação em PHP (Laravel) e bancos de dados MySQL. Atualmente ampliando conhecimentos em frameworks modernos como React e Vue.js para retomar atuação na área de programação.",
+        tecnologias: [
+            { nome: "React", icone: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" },
+            { nome: "Bootstrap", icone: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original.svg" },
+            { nome: "Vue.js", icone: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vuejs/vuejs-original.svg" },
+        ]
+    },
+    {
+        id: "edtpy",
+        titulo: "Edtpy",
+        imagem: "/assets/images/edtpy.png", 
+        resumo: "Loja de produtos para rodeios, construido em wordpress.",
+        descricao: "Desenvolvedora Full Stack com experiência em sistemas ERP e vitrines virtuais, com forte atuação em PHP (Laravel) e bancos de dados MySQL. Atualmente ampliando conhecimentos em frameworks modernos como React e Vue.js para retomar atuação na área de programação.",
+        tecnologias: [
+            { nome: "React", icone: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" },
+            { nome: "Bootstrap", icone: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original.svg" },
+            { nome: "Vue.js", icone: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vuejs/vuejs-original.svg" },
+        ]
+    },
+];
+
+export default projetosData;
