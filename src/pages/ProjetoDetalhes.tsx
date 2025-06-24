83c704b920561d842b8fa8e9c4a71fb93e7399b7
Ana Carolina dos Anjos <anacarolinaaps@gmail.com>
2025-06-24 17:08:13 -0300

    [chore] Ajustado para passar infos para ProjetoDetalhe

--- src/pages/ProjetoDetalhes.tsx
+++ src/pages/ProjetoDetalhes.tsx
@@ -1,9 +1,20 @@
+import { useParams } from 'react-router-dom';
+import projetosData from '../data/projetos'; // ajuste o caminho conforme necessário
+
 function ProjetoDetalhes() {
+  const { id } = useParams();
+  const projeto = projetosData.find(proj => proj.id === id);
+
+  if (!projeto) {
+    return <h2>Projeto não encontrado</h2>;
+  }
+
   return (
     <> 
       <div className="row">
         <div className="col">
-          <h1 className='text-3xl heading-main text-secondary'>Projetos</h1>
+          <h1 className='text-3xl heading-main text-secondary'>Detalhes Projetos</h1>
+          <h2>{projeto.titulo}</h2>
         </div>
       </div>
     </>
