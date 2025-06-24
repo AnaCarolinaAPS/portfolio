2e507ddcd897d3a4c8bf087701f4c0fa9ff255ae
Ana Carolina dos Anjos <anacarolinaaps@gmail.com>
2025-06-24 17:07:35 -0300

    [refactor] (Home) Ajustes nas declarações

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Inicio from './pages/Home';
 import Sobre from './pages/Sobre';
 import Projetos from './pages/Projetos';
+import ProjetoDetalhes from './pages/ProjetoDetalhes';
 import Header from './components/Header/Header';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<Inicio />} />
           <Route path="/sobre" element={<Sobre />} />
           <Route path="/projetos" element={<Projetos />} />
+          <Route path="/projects/:id" element={<ProjetoDetalhes />} />
         </Routes>
       </div>
     </Router>
