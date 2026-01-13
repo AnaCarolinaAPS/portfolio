462a75f95843c44bf8f9434e3fc7094738ee796c
Ana Carolina <anacarolinaaps@gmail.com>
2026-01-13 19:19:42 -0300

    [feat] (Sobremim) Changed CV.pdf
    - Changed from Ana-CV.pdf to new CV Ana Carolina dos Anjos Pereira da Silva - Dev Front-End.pdf

--- src/data/sobremim.ts
+++ src/data/sobremim.ts
@@ -23,7 +23,7 @@ export interface SobremimDataType {
 
 const sobremimData: SobremimDataType = {
     resumo: "Minha trajetória profissional na tecnologia começou em 2010 e ao longo dos anos tive a oportunidade de trabalhar com diversas áreas, como ensino, desenvolvimento desktop e web, além de testes de software e análise. Tive oportunidade de trabalhar com diversas linguagens, incluindo tecnologias proprietárias.\nNos últimos sete anos, desenvolvi um sistema ERP completo em PHP (laravel) apesar da minha principal aérea de atuação estar focada na administrativa e financeiro da empresa em que trabalho. Pude com isso, informatizar 100% os processos logísticos e financeiros da empresa.\nNo último ano venho me dedicando ao front-end, aprimorando minhas habilidades em React e VueJS. Estou muito animada para atuar como desenvolvedora júnior. Amo aprender coisas novas, tanto no âmbito profissional quanto pessoal. Compartilhar conhecimento e construir soluções que facilitem processos é algo que considero muito satisfatório, seja causando pequenos ou grandes impactos.",  
-    linkcv: "/assets/Ana-CV.pdf",
+    linkcv: "/assets/Ana Carolina dos Anjos Pereira da Silva - Dev Front-End.pdf",
     itens: [
         { nome: "linkedin", linkContato:"https://www.linkedin.com/in/ana-carolina-aps/", icone: "/assets/icons/brand-linkedin.svg" },
         { nome: "github", linkContato:"https://github.com/AnaCarolinaAPS", icone: "/assets/icons/brand-github.svg" },
