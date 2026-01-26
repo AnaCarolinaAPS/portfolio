40dbc8c86b64428037e923ed94efddae3814bab1
Ana Carolina <anacarolinaaps@gmail.com>
2026-01-25 21:55:26 -0300

    [feat] Create Information @Type

---
+++ src/@types/Information.ts
@@ -0,0 +1,11 @@
+export type Information = {
+    headline: string,
+    title: string,
+    summary: string,
+    about_me: string,
+    locale: string,
+}
+
+export type ApiGetInformation = {
+    information: Information
+}
